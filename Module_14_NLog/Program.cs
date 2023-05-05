5d4768e65e13edf88486e1bf9a0058676afe40bb
Waken <votrevincentwaken@gmail.com>
2023-05-06 00:47:55 +0300

    First Commit

---
+++ Module_14_NLog/Program.cs
@@ -0,0 +1,126 @@
+﻿using NLog;
+using NLog.Conditions;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+using System.Text;
+
+namespace Module_14_NLog
+{   
+    // https://github.com/nlog/nlog/wiki/Tutorial#Configure-NLog-Targets-for-output
+    // https://github.com/nlog/nlog/wiki/Configure-from-code
+    // https://yougame.biz/threads/200323/
+    // Layouts https://nlog-project.org/config/?tab=layouts
+    // Targets https://nlog-project.org/config/?tab=targets
+    // Layout renderers https://nlog-project.org/config/?tab=layout-renderers
+
+    internal class Program
+    {
+        public static Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+
+            #region NLog Initializator
+
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var Target = new ColoredConsoleTarget()
+            {
+                Layout = @"[${logger}/${uppercase: ${level}}] >> ${message}"
+
+            };
+            config.AddRule(LogLevel.Trace, LogLevel.Info, Target);
+
+            NLog.LogManager.Configuration = config;
+
+
+            #endregion NLog Initializator
+
+            try
+            {
+                string nameFile = "test.txt";
+                StreamWriter file = new StreamWriter(nameFile, false);
+                string str = "Hello World\n 2023 World Hello\n Module 14 Nlog";
+                file.WriteLine(str);
+                file.Close();
+
+
+
+                int choice = 0;
+                do
+                {
+                    Console.WriteLine("1. A String with One Lowercase English letter");
+                    Console.WriteLine("2. String with one number");
+                    Console.WriteLine("3. String with one capital English letter");
+                    Console.WriteLine("0. Exit");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (choice)
+                    {
+                        case 0:
+                            return;
+                        case 1:
+                            {
+                                ReadFile_txt(nameFile, 97, 122);
+                                Logger.Info("Вывод всех предложений с маленькой англ буквой");
+                                break;
+                            }
+                        case 2:
+                            {
+                                ReadFile_txt(nameFile, 48, 57);
+                                Logger.Info("Вывод всех предложений с цифрой");
+                                break;
+                            }
+                        case 3:
+                            {
+                                ReadFile_txt(nameFile, 65, 90);
+                                Logger.Info("Вывод всех предложений с большой англ буквой");
+                                break;
+                            }
+                        default:
+                            {
+                                Logger.Info("Была введена неверная операция");
+                                break;
+                            }
+
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+
+
+                } while (choice != 0);
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Error " + ex.Message);
+                Logger.Fatal("Fatal Error! " + ex);
+            }
+        }
+
+        static void ReadFile_txt(string NameFile, int start, int end)
+        {
+
+            string line = null;
+            bool flag = false;
+            StreamReader reader = new StreamReader(NameFile, Encoding.UTF8);
+            while (reader.Peek() >= 0)
+            {
+                line = reader.ReadLine();
+                char[] chars = line.ToCharArray();
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (chars[i] >= start && chars[i] <= end) flag = true;
+                }
+                if (flag)
+                {
+                    Console.WriteLine(line);
+                    flag = false;
+                }
+            }
+            reader.Close();
+        }
+    
+    }
+}
---
+++ secondTask/Program.cs
@@ -0,0 +1,86 @@
+﻿using NLog;
+using NLog.Conditions;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+using System.Text;
+using Faker;
+using NLog.Fluent;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Serialization;
+
+namespace Module_14_NLog
+{
+    // https://github.com/nlog/nlog/wiki/Tutorial#Configure-NLog-Targets-for-output
+    // https://github.com/nlog/nlog/wiki/Configure-from-code
+    // https://yougame.biz/threads/200323/
+    // Layouts https://nlog-project.org/config/?tab=layouts
+    // Targets https://nlog-project.org/config/?tab=targets
+    // Layout renderers https://nlog-project.org/config/?tab=layout-renderers
+
+    internal class Program
+    {
+        public static Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+
+            #region NLog Initializator
+
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var Target = new ColoredConsoleTarget()
+            {
+                Layout = @"[${logger}/${uppercase: ${level}}] >> ${message}"
+
+            };
+            config.AddRule(LogLevel.Trace, LogLevel.Info, Target);
+
+            NLog.LogManager.Configuration = config;
+
+
+            #endregion NLog Initializator
+            for (int i = 0; i < 10; i++)
+            {
+                var user = new FakeUser();
+                user.Name = Faker.Name.First();
+                user.Surname = Faker.Name.Last();
+                user.PhoneNumber = Faker.Phone.Number();
+                user.Email = Faker.Internet.Email();
+                user.address = Faker.Address.StreetAddress();
+
+                var currentDirectory = System.IO.Directory.GetCurrentDirectory();
+                Console.WriteLine($"Fake User: {user}");
+                WriteToFile(user);
+            };
+        }
+        static void WriteToFile(FakeUser user)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(FakeUser));
+            FileStream file = new FileStream("File.xml", FileMode.Append);
+            serializer.Serialize(file, user);
+            file.Close();
+
+        }
+    }
+
+    public class FakeUser
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+            public string address { get; set; }
+        public override string ToString()
+        {
+            return "\nName: " + Name +
+                "\nSurname: " + Surname +
+                "\nPhone Number: " + PhoneNumber +
+                "\nEmail: " + Email +
+                "\nAddress" + address + "\n";
+        }
+    }
+
+        
+}
